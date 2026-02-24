83cf1ccb365d24bd8112f4f76f3a409924b0e7d0
Trey Guckian <24757349+tgucks@users.noreply.github.com>
2026-02-24 14:04:00 -0600

    Add golang lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -47,6 +47,116 @@ require("lazy").setup({
       vim.cmd("colorscheme dracula")
     end,
   },
+  -- LSP: Mason installs language servers automatically
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig", "hrsh7th/cmp-nvim-lsp" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "gopls" },
+        automatic_installation = true,
+      })
+
+      -- Keymaps + auto-hover, scoped to buffers with an active LSP
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
+        callback = function(args)
+          local buf = args.buf
+          local function map(keys, func, desc)
+            vim.keymap.set("n", keys, func, { buffer = buf, desc = desc })
+          end
+
+          map("gd",         vim.lsp.buf.definition,     "Go to definition")
+          map("gD",         vim.lsp.buf.declaration,    "Go to declaration")
+          map("gr",         vim.lsp.buf.references,     "References")
+          map("gi",         vim.lsp.buf.implementation, "Go to implementation")
+          map("K",          vim.lsp.buf.hover,          "Hover docs")
+          map("<leader>rn", vim.lsp.buf.rename,         "Rename symbol")
+          map("<leader>ca", vim.lsp.buf.code_action,    "Code action")
+          map("[d",         vim.diagnostic.goto_prev,   "Prev diagnostic")
+          map("]d",         vim.diagnostic.goto_next,   "Next diagnostic")
+
+          -- Show hover docs automatically after cursor is still for updatetime ms
+          vim.api.nvim_create_autocmd("CursorHold", {
+            buffer = buf,
+            callback = function() vim.lsp.buf.hover() end,
+          })
+        end,
+      })
+
+      -- Format Go files on save
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+          vim.lsp.buf.format({ async = false })
+        end,
+      })
+
+      -- updatetime controls how long the cursor must be still before CursorHold fires
+      vim.opt.updatetime = 1000
+
+      -- Configure gopls using the new nvim 0.11 API
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      vim.lsp.config("gopls", {
+        capabilities = capabilities,
+        settings = {
+          gopls = {
+            analyses = { unusedparams = true, shadow = true },
+            staticcheck = true,
+          },
+        },
+      })
+      vim.lsp.enable("gopls")
+    end,
+  },
+
+  -- Autocompletion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      cmp.setup({
+        snippet = {
+          expand = function(args) luasnip.lsp_expand(args.body) end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"]      = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"]     = cmp.mapping(function(fallback)
+            if cmp.visible() then cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
+            else fallback() end
+          end, { "i", "s" }),
+          ["<S-Tab>"]   = cmp.mapping(function(fallback)
+            if cmp.visible() then cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then luasnip.jump(-1)
+            else fallback() end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
+
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
