68190bd958acebc0b6d8bd2b16e88ae318a93852
Trey Guckian <24757349+tgucks@users.noreply.github.com>
2026-02-26 15:43:26 -0600

    shortcut to expand json into multiline

--- nvim/init.lua
+++ nvim/init.lua
@@ -61,6 +61,19 @@ vim.keymap.set('v', '<leader>/', 'gc',  { remap = true, desc = "Toggle comment" 
 -- Paste in visual mode without clobbering the yank register
 vim.keymap.set('x', 'p', '"_dP', { desc = "Paste without overwriting register" })
 
+-- Expand inline collection to multiline (<leader>J, opposite of J which joins)
+vim.keymap.set("n", "<leader>J", function()
+  local lnum = vim.fn.line(".")
+  local line = vim.api.nvim_get_current_line()
+  local indent = line:match("^(%s*)")
+  local inner = indent .. string.rep(" ", vim.fn.shiftwidth())
+  local result = line
+  result = result:gsub("([{%[])%s*", "%1\n" .. inner)
+  result = result:gsub(",%s*", ",\n" .. inner)
+  result = result:gsub("%s*([%]}])", "\n" .. indent .. "%1")
+  vim.api.nvim_buf_set_lines(0, lnum - 1, lnum, false, vim.split(result, "\n"))
+end, { desc = "Expand inline to multiline" })
+
 -- Close all floating windows and suppress auto-reopen until cursor moves
 vim.keymap.set("n", "<Esc>", function()
   vim.b[vim.api.nvim_get_current_buf()].lsp_float_state = "dismissed"
