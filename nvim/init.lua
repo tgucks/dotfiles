a94e120052f49eecda489a8d29204ea7cb55fbba
Trey Guckian <24757349+tgucks@users.noreply.github.com>
2026-02-26 15:25:40 -0600

    add lualina

--- nvim/init.lua
+++ nvim/init.lua
@@ -120,6 +120,36 @@ require("lazy").setup({
       })
     end
   },
+  -- Status line
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
+        options = { theme = "catppuccin" },
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {
+            'branch',
+            {
+              'diff',
+              source = function()
+                local gs = vim.b.gitsigns_status_dict
+                if gs then
+                  return { added = gs.added, modified = gs.changed, removed = gs.removed }
+                end
+              end,
+            },
+            'diagnostics',
+          },
+          lualine_c = {'filename'},
+          lualine_x = {'encoding', 'fileformat', 'filetype'},
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        },
+      })
+    end,
+  },
   -- {
   --   "Mofiqul/dracula.nvim",
   --   priority = 1000,
