3477f0ec7df07bc7246b3385b14146ae82824cd3
Trey Guckian <24757349+tgucks@users.noreply.github.com>
2026-02-26 14:44:35 -0600

    update keymaps for telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -333,13 +333,20 @@ require("lazy").setup({
   -- Fuzzy finder
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
     config = function()
+      require("telescope").setup({
+        extensions = {
+          fzf = {},
+        },
+      })
+      require("telescope").load_extension("fzf")
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>f", builtin.find_files,  { desc = "Find files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep,   { desc = "Live grep" })
-      vim.keymap.set("n", "<leader>fb", builtin.buffers,     { desc = "Find buffers" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags,   { desc = "Help tags" })
+      vim.keymap.set("n", "<leader>f",  builtin.find_files,  { desc = "Find files" })
+      vim.keymap.set("n", "<leader>r", builtin.live_grep,   { desc = "Live grep" })
     end,
   },
   -- Scrollbar with git and diagnostic markers
